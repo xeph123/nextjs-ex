5498f2360c543c761691e54ee1ff34480b3c4ae6
유남욱 <xeph123@naver.com>
2025-08-02 21:43:51 +0900

    챕터 9 완료

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,4 @@
 import postgres from 'postgres';
-import { sql } from '@vercel/postgres';
 import {
   CustomerField,
   CustomersTableType,
@@ -17,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
